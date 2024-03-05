09c9284702e0d40c2ae1fc91fb1c9c6345ec4cdf
Mohd Ashraf <98030979+mohdashraf1234@users.noreply.github.com>
2024-03-05 14:46:12 +0530

    Update index.js
    Add touch control

--- js/index.js
+++ js/index.js
@@ -1,120 +1,3 @@
-
-let inputDir = {x: 0, y: 0}; 
-const foodSound = new Audio('./music/food.mp3');
-const gameOverSound = new Audio('./music/gameover.mp3');
-const moveSound = new Audio('./music/straner-things-124008.mp3');
-const musicSound = new Audio('./music/music.mp3');
-let speed = 19;
-let score = 0;
-let lastPaintTime = 0;
-let snakeArr = [
-    {x: 13, y: 15}
-];
-
-let food = {x: 6, y: 7}; 
-
-
-function main(ctime) {
-    window.requestAnimationFrame(main);
-   
-    if((ctime - lastPaintTime)/1000 < 1/speed){
-        return;
-    }
-    lastPaintTime = ctime;
-    gameEngine();
-}
-
-function isCollide(snake) {
-    
-    for (let i = 1; i < snakeArr.length; i++) {
-        if(snake[i].x === snake[0].x && snake[i].y === snake[0].y){
-            return true;
-        }
-    }
-    
-    if(snake[0].x >= 18 || snake[0].x <=0 || snake[0].y >= 18 || snake[0].y <=0){
-        return true;
-    }
-        
-    return false;
-}
-
-function gameEngine(){
- 
-    if(isCollide(snakeArr)){
-        gameOverSound.play();
-        musicSound.pause();
-        inputDir =  {x: 0, y: 0}; 
-        document.getElementById('outMessage').innerText = "You are out";
-        setTimeout(() => {
-            document.getElementById('outMessage').innerText = "";
-        }, 2000); 
-        snakeArr = [{x: 13, y: 15}];
-        musicSound.play();
-        score = 0; 
-    }
-
-   
-    if(snakeArr[0].y === food.y && snakeArr[0].x ===food.x){
-        foodSound.play();
-        score += 1;
-        if(score>hiscoreval){
-            hiscoreval = score;
-            localStorage.setItem("hiscore", JSON.stringify(hiscoreval));
-            hiscoreBox.innerHTML = "HiScore: " + hiscoreval;
-        }
-        scoreBox.innerHTML = "Score: " + score;
-        snakeArr.unshift({x: snakeArr[0].x + inputDir.x, y: snakeArr[0].y + inputDir.y});
-        let a = 2;
-        let b = 16;
-        food = {x: Math.round(a + (b-a)* Math.random()), y: Math.round(a + (b-a)* Math.random())}
-    }
-
-  
-    for (let i = snakeArr.length - 2; i>=0; i--) { 
-        snakeArr[i+1] = {...snakeArr[i]};
-    }
-
-    snakeArr[0].x += inputDir.x;
-    snakeArr[0].y += inputDir.y;
-
-   
-   
-    board.innerHTML = "";
-    snakeArr.forEach((e, index)=>{
-        snakeElement = document.createElement('div');
-        snakeElement.style.gridRowStart = e.y;
-        snakeElement.style.gridColumnStart = e.x;
-
-        if(index === 0){
-            snakeElement.classList.add('head');
-        }
-        else{
-            snakeElement.classList.add('snake');
-        }
-        board.appendChild(snakeElement);
-    });
-   
-    foodElement = document.createElement('div');
-    foodElement.style.gridRowStart = food.y;
-    foodElement.style.gridColumnStart = food.x;
-    foodElement.classList.add('food')
-    board.appendChild(foodElement);
-}
-
-
-musicSound.play();
-let hiscore = localStorage.getItem("hiscore");
-if(hiscore === null){
-    hiscoreval = 0;
-    localStorage.setItem("hiscore", JSON.stringify(hiscoreval))
-}
-else{
-    hiscoreval = JSON.parse(hiscore);
-    hiscoreBox.innerHTML = "HiScore: " + hiscore;
-}
-
-window.requestAnimationFrame(main);
 window.addEventListener('keydown', e =>{
     inputDir = {x: 0, y: 1} 
     moveSound.play();
@@ -146,3 +29,48 @@ window.addEventListener('keydown', e =>{
             break;
     }
 });
+
+// Add touch controls for mobile devices
+const touchArea = document.getElementById('board');
+let touchStartX = 0;
+let touchStartY = 0;
+
+touchArea.addEventListener('touchstart', handleTouchStart, false);
+touchArea.addEventListener('touchmove', handleTouchMove, false);
+
+function handleTouchStart(event) {
+    touchStartX = event.touches[0].clientX;
+    touchStartY = event.touches[0].clientY;
+}
+
+function handleTouchMove(event) {
+    event.preventDefault();
+    const touchEndX = event.touches[0].clientX;
+    const touchEndY = event.touches[0].clientY;
+
+    const deltaX = touchEndX - touchStartX;
+    const deltaY = touchEndY - touchStartY;
+
+    // Adjust sensitivity based on your game's requirements
+    if (Math.abs(deltaX) > Math.abs(deltaY)) {
+        if (deltaX > 0) {
+            // Swipe right
+            inputDir.x = 1;
+            inputDir.y = 0;
+        } else {
+            // Swipe left
+            inputDir.x = -1;
+            inputDir.y = 0;
+        }
+    } else {
+        if (deltaY > 0) {
+            // Swipe down
+            inputDir.x = 0;
+            inputDir.y = 1;
+        } else {
+            // Swipe up
+            inputDir.x = 0;
+            inputDir.y = -1;
+        }
+    }
+}
